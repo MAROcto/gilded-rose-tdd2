b3bad695baaaff781c124da73c80ac73fc8d2b49
MARO <mathieu.tammaro@octo.com>
2022-11-07 12:21:55 +0100

    WIP to change machine

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,39 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+describe("Gilded Rose", () => {
+  describe("Items", () => {
+    it("Should have a sellIn value", () => {
+      //Given
+      const name = "testItem";
+      const sellInValue = 10;
+      const qualityValue = 5;
+      //When
+      var item = new Item(name, sellInValue, qualityValue);
+      //Then
+      expect(item.sellIn).toBe(sellInValue);
+    });
+    it("Should have a quality value", () => {
+      //Given
+      const name = "testItem";
+      const sellInValue = 10;
+      const qualityValue = 5;
+      //When
+      var item = new Item(name, sellInValue, qualityValue);
+      //Then
+      expect(item.quality).toBe(qualityValue);
+    });
+    it("Should have a name", () => {
+      //Given
+      const name = "testItem";
+      const sellInValue = 10;
+      const qualityValue = 5;
+      //When
+      var item = new Item(name, sellInValue, qualityValue);
+      //Then
+      expect(item.name).toBe(name);
+    });
+  });
+  describe("UpdateRules", () => {
+    it("Should decrease  ");
   });
 });
